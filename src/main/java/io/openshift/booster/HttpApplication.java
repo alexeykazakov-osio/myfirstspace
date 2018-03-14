8037ff90f4d589c77744d2570b893d6160818086
alexeykazakov-osio <igelsss+osio@gmail.com>
2018-03-14 03:11:25 +0000

    Minor change

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -9,6 +9,7 @@ import io.vertx.ext.web.handler.StaticHandler;
 
 import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
+// Version 2
 public class HttpApplication extends AbstractVerticle {
 
   protected static final String template = "Hello, %s!";
